c8af6228503f358a7eacf709fd5e566209be6f25
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-05-20 02:38:48 +0900

    Update Queen-s-Attack-II.cpp

--- Queen-s-Attack-II.cpp
+++ Queen-s-Attack-II.cpp
@@ -16,31 +16,31 @@ int queensAttack(int n, int k, int r_q, int c_q, vector<vector<int>> obstacles)
 	{
 		if (OB[0] == X)
 		{
-			if (OB[1] > Y  &&  OB[1] - Y - 1 < E)
+			if (OB[1] > Y   &&   OB[1] - Y - 1 < E)
 				E = OB[1] - Y - 1;
-			else if (OB[1] < Y && Y - OB[1] - 1 < W)
+			else if (OB[1] < Y   &&   Y - OB[1] - 1 < W)
 				W = Y - OB[1] - 1;
 		}
 		else if (OB[1] == Y)
 		{
-			if (OB[0] > X && OB[0] - X - 1 < N)
+			if (OB[0] > X   &&   OB[0] - X - 1 < N)
 				N = OB[0] - X - 1;
-			else if (OB[0] < X && X - OB[0] - 1 < S)
+			else if (OB[0] < X   &&   X - OB[0] - 1 < S)
 				S = X - OB[0] - 1;
 		}
-		else if (OB[0] - X == OB[1] - Y && OB[0] - X > 0 && OB[0] - X - 1 < NE)
+		else if (OB[0] - X == OB[1] - Y   &&   OB[0] - X > 0   &&   OB[0] - X - 1 < NE)
 		{
 			NE = OB[0] - X - 1;
 		}
-		else if (X - OB[0] == Y - OB[1] && X - OB[0] > 0 && X - OB[0] - 1 < SW)
+		else if (X - OB[0] == Y - OB[1]   &&   X - OB[0] > 0   &&   X - OB[0] - 1 < SW)
 		{
 			SW = X - OB[0] - 1;
 		}
-		else if (OB[0] - X == -(OB[1] - Y) && OB[0] - X > 0 && OB[0] - X - 1 < NW)
+		else if (OB[0] - X == -(OB[1] - Y)   &&   OB[0] - X > 0   &&   OB[0] - X - 1 < NW)
 		{
 			NW = OB[0] - X - 1;
 		}
-		else if (X - OB[0] == -(Y - OB[1]) && X - OB[0] > 0 && X - OB[0] - 1 < SE)
+		else if (X - OB[0] == -(Y - OB[1])   &&   X - OB[0] > 0   &&   X - OB[0] - 1 < SE)
 		{
 			SE = X - OB[0] - 1;
 		}
