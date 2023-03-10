71be3ec1371adf935703fa903a60753d19df08ed
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-03-11 02:27:21 +0900

    Update Queen-s-Attack-II.cpp

--- Queen-s-Attack-II.cpp
+++ Queen-s-Attack-II.cpp
@@ -69,3 +69,61 @@ int queensAttack(int n, int k, int r_q, int c_q, vector<vector<int>> obstacles)
 	return (N + S + W + E + NE + NW + SE + SW);
 }
 
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int k = stoi(first_multiple_input[1]);
+
+    string second_multiple_input_temp;
+    getline(cin, second_multiple_input_temp);
+
+    vector<string> second_multiple_input = split(rtrim(second_multiple_input_temp));
+
+    int r_q = stoi(second_multiple_input[0]);
+
+    int c_q = stoi(second_multiple_input[1]);
+
+    vector<vector<int>> obstacles(k);
+
+    for (int i = 0; i < k; i++) {
+        obstacles[i].resize(2);
+
+        string obstacles_row_temp_temp;
+        getline(cin, obstacles_row_temp_temp);
+
+        vector<string> obstacles_row_temp = split(rtrim(obstacles_row_temp_temp));
+
+        for (int j = 0; j < 2; j++) {
+            int obstacles_row_item = stoi(obstacles_row_temp[j]);
+
+            obstacles[i][j] = obstacles_row_item;
+        }
+    }
+
+    int result = queensAttack(n, k, r_q, c_q, obstacles);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
