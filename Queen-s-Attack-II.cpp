5c93819e405b0fa8345f5b5a6764e12520eb6da1
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-03-13 02:23:12 +0900

    Update Queen-s-Attack-II.cpp

--- Queen-s-Attack-II.cpp
+++ Queen-s-Attack-II.cpp
@@ -20,53 +20,53 @@ vector<string> split(const string &);
 
 int queensAttack(int n, int k, int r_q, int c_q, vector<vector<int>> obstacles)
 {
-	int X = r_q;
-	int Y = c_q;
+    int X = r_q;
+    int Y = c_q;
 
-	int E = n - Y;
-	int W = Y - 1;
-	int N = n - X;
-	int S = X - 1;
-	int NE = std::min(N, E);
-	int NW = std::min(N, W);
-	int SE = std::min(S, E);
-	int SW = std::min(S, W);
+    int E = n - Y;
+    int W = Y - 1;
+    int N = n - X;
+    int S = X - 1;
+    int NE = std::min(N, E);
+    int NW = std::min(N, W);
+    int SE = std::min(S, E);
+    int SW = std::min(S, W);
 
-	for (vector<int> OB : obstacles)
-	{
-		if (OB[0] == X)
-		{
-			if (OB[1] > Y   &&   OB[1] - Y - 1 < E)
-				E = OB[1] - Y - 1;
-			else if (OB[1] < Y   &&   Y - OB[1] - 1 < W)
-				W = Y - OB[1] - 1;
-		}
-		else if (OB[1] == Y)
-		{
-			if (OB[0] > X   &&   OB[0] - X - 1 < N)
-				N = OB[0] - X - 1;
-			else if (OB[0] < X   &&   X - OB[0] - 1 < S)
-				S = X - OB[0] - 1;
-		}
-		else if (OB[0] - X == OB[1] - Y   &&   OB[0] - X > 0   &&   OB[0] - X - 1 < NE)
-		{
-			NE = OB[0] - X - 1;
-		}
-		else if (X - OB[0] == Y - OB[1]   &&   X - OB[0] > 0   &&   X - OB[0] - 1 < SW)
-		{
-			SW = X - OB[0] - 1;
-		}
-		else if (OB[0] - X == -(OB[1] - Y)   &&   OB[0] - X > 0   &&   OB[0] - X - 1 < NW)
-		{
-			NW = OB[0] - X - 1;
-		}
-		else if (X - OB[0] == -(Y - OB[1])   &&   X - OB[0] > 0   &&   X - OB[0] - 1 < SE)
-		{
-			SE = X - OB[0] - 1;
-		}
-	}
+    for (vector<int> OB : obstacles)
+    {
+        if (OB[0] == X)
+        {
+            if (OB[1] > Y   &&   OB[1] - Y - 1 < E)
+                E = OB[1] - Y - 1;
+            else if (OB[1] < Y   &&   Y - OB[1] - 1 < W)
+                W = Y - OB[1] - 1;
+        }
+        else if (OB[1] == Y)
+        {
+            if (OB[0] > X   &&   OB[0] - X - 1 < N)
+                N = OB[0] - X - 1;
+            else if (OB[0] < X   &&   X - OB[0] - 1 < S)
+                S = X - OB[0] - 1;
+        }
+        else if (OB[0] - X == OB[1] - Y   &&   OB[0] - X > 0   &&   OB[0] - X - 1 < NE)
+        {
+            NE = OB[0] - X - 1;
+        }
+        else if (X - OB[0] == Y - OB[1]   &&   X - OB[0] > 0   &&   X - OB[0] - 1 < SW)
+        {
+            SW = X - OB[0] - 1;
+        }
+        else if (OB[0] - X == -(OB[1] - Y)   &&   OB[0] - X > 0   &&   OB[0] - X - 1 < NW)
+        {
+            NW = OB[0] - X - 1;
+        }
+        else if (X - OB[0] == -(Y - OB[1])   &&   X - OB[0] > 0   &&   X - OB[0] - 1 < SE)
+        {
+            SE = X - OB[0] - 1;
+        }
+    }
 
-	return (N + S + W + E + NE + NW + SE + SW);
+    return (N + S + W + E + NE + NW + SE + SW);
 }
 
 int main()
@@ -126,4 +126,32 @@ string ltrim(const string &str) {
     );
 
     return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
 }
